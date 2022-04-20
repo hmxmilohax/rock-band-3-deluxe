e7aba8919e83466b3e78243f33c4ac3c7db0d846
Puyo <hi@puyo.xyz>
2022-04-20 06:57:28 +0200

    Initial Linux scripts

---
+++ _build_xbox.sh
@@ -0,0 +1,2 @@
+git pull https://github.com/jnackmclain/rock-band-3-deluxe main
+dependencies/arkhelper dir2ark ./_ark ./_build/xbox/gen -n "patch_xbox" -e -v 6
---
+++ _init_repo.sh
@@ -0,0 +1,5 @@
+git init
+git pull https://github.com/jnackmclain/rock-band-3-deluxe main
+chmod +x dependencies/*
+chmod +x _build_ps3.sh
+chmod +x _build_xbox.sh
