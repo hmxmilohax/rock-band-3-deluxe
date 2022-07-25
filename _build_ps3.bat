02394f9673cd548d93f6f6dfbce1750d981e029e
rjkiv <rjkiv@users.noreply.github.com>
2022-07-25 17:55:35 -0400

    output now visible on _build scripts

--- _build_ps3.bat
+++ _build_ps3.bat
@@ -1,2 +1,3 @@
 git pull https://github.com/jnackmclain/rock-band-3-deluxe main
 "%~dp0dependencies/arkhelper" dir2ark "%~dp0\_ark" "%~dp0\_build\ps3\USRDIR\gen" -n "patch_ps3" -e -v 6
+pause
--- _build_xbox.bat
+++ _build_xbox.bat
@@ -1,2 +1,3 @@
 git pull https://github.com/jnackmclain/rock-band-3-deluxe main
-"%~dp0dependencies/arkhelper" dir2ark "%~dp0\_ark" "%~dp0\_build\xbox\gen" -n "patch_xbox" -e -v 6
+"%~dp0\dependencies/arkhelper" dir2ark "%~dp0\_ark" "%~dp0\_build\xbox\gen" -n "patch_xbox" -e -v 6
+pause
