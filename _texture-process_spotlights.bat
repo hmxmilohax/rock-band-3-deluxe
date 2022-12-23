9a74f7c9ad2fe7c292beb6e6a10bbcb4c9dc20cf
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-12-22 20:03:49 -0600

    update spotlight texture script. add run_xenia

--- _texture-process_spotlights.bat
+++ _texture-process_spotlights.bat
@@ -14,9 +14,9 @@ move "%~dp0custom_textures\spotlights\spotlights.dta" "%~dp0_ark\ui\track\spotli
 "%~dp0dependencies/sed.exe" -i -e "s/\t/ /g" "%~dp0_ark\ui\track\spotlights\spotlights.dta"
 "%~dp0dependencies/sed.exe" -i -e "s/\"rb2_bass_superstreak\"/          \"rb2_bass_superstreak\"/g" "%~dp0_ark\ui\track\spotlights\spotlights.dta"
 %~dp0dependencies/sed.exe -i -e "/^          \"rb2_bass_superstreak\"/{r %~dp0_ark\ui\track\spotlights\spotlights.dta" -e "d;}" %~dp0_ark\ui\overshell\slot_states.dta
-FOR /F "tokens=*" %%G IN ('dir /b *.png') DO "%~dp0dependencies/magick/magick.exe" convert "%~dp0custom_textures\spotlights/%%G" -resize 256x512! -filter Box -alpha set -background none -channel A -evaluate multiply 0.5 +channel "%~dp0custom_textures\spotlights\%%~nG.png"
-FOR /F "tokens=*" %%G IN ('dir /b *.jpg') DO "%~dp0dependencies/magick/magick.exe" convert "%~dp0custom_textures\spotlights/%%G" -resize 256x512! -filter Box -alpha set -background none -channel A -evaluate multiply 0.5 +channel "%~dp0custom_textures\spotlights\%%~nG.png"
-FOR /F "tokens=*" %%G IN ('dir /b *.bmp') DO "%~dp0dependencies/magick/magick.exe" convert "%~dp0custom_textures\spotlights/%%G" -resize 256x512! -filter Box -alpha set -background none -channel A -evaluate multiply 0.5 +channel "%~dp0custom_textures\spotlights\%%~nG.png"
+FOR /F "tokens=*" %%G IN ('dir /b *.png') DO "%~dp0dependencies/magick/magick.exe" convert "%~dp0custom_textures\spotlights/%%G" -resize 256x512! -filter Box -alpha set -background none +channel "%~dp0custom_textures\spotlights\%%~nG.png"
+FOR /F "tokens=*" %%G IN ('dir /b *.jpg') DO "%~dp0dependencies/magick/magick.exe" convert "%~dp0custom_textures\spotlights/%%G" -resize 256x512! -filter Box -alpha set -background none +channel "%~dp0custom_textures\spotlights\%%~nG.png"
+FOR /F "tokens=*" %%G IN ('dir /b *.bmp') DO "%~dp0dependencies/magick/magick.exe" convert "%~dp0custom_textures\spotlights/%%G" -resize 256x512! -filter Box -alpha set -background none +channel "%~dp0custom_textures\spotlights\%%~nG.png"
 FOR /F "tokens=*" %%G IN ('dir /b *.png') DO "%~dp0dependencies/superfreq.exe" png2tex "%~dp0custom_textures\spotlights/%%G" "%~dp0_ark\ui\track\spotlights\gen\%%~nG.png_xbox" --platform x360 --miloVersion 26
 cd "%~dp0_ark/ui/track/spotlights/gen"
 FOR /F "tokens=*" %%G IN ('dir /b *.png_xbox') DO python "%~dp0dependencies/swap_rb_art_bytes.py" "%~dp0_ark/ui/track/spotlights/gen/%%G" "%~dp0_ark/ui/track/spotlights/gen/%%~nG.png_ps3"
