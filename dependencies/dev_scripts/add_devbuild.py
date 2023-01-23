db202da33b5f96eab21bfae3974dc2f3b3219811
rjkiv <rjkiv@users.noreply.github.com>
2023-01-23 16:13:16 -0700

    tweak devbuild script

--- dependencies/dev_scripts/add_devbuild.py
+++ dependencies/dev_scripts/add_devbuild.py
@@ -1,14 +1,21 @@
 # add_devbuild.py
 from pathlib import Path
 import sys
+import git
 
 def main():
     if len(sys.argv) != 2:
         print("no devbuild provided")
     else:
         commit = sys.argv[1]
+        print(f"Commit: {commit}")
         cwd = Path().absolute() # current working directory (dev_scripts)
         root_dir = cwd.parents[1] # root directory of the repo
+
+        repo = git.Repo(search_parent_directories=True)
+        sha = repo.head.commit.hexsha
+        short_sha = repo.git.rev_parse(sha, short=4)
+        print(f"Short sha: {short_sha}")
         print(root_dir)
         # sed -i -e "s/devbuild/"$GITHUB_SHA_SHORT"/g" _ark/ui/locale/*/locale_updates_keep.dta
         for locale in root_dir.joinpath("_ark/ui/locale").glob("*/locale_updates_keep.dta"):
