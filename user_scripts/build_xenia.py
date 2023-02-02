87622e1df6928faa8e2457404a4877e14a3bd1b6
rjkiv <rjkiv@users.noreply.github.com>
2023-02-01 19:32:58 -0700

    appease jnack

--- user_scripts/build_xenia.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append("../dependencies/dev_scripts")
-
-from build_ark import build_patch_ark
-
-build_patch_ark(True)
-print("Ready to run RB3DX in Xenia.")
--- user_scripts/run_xenia.py
+++ user_scripts/run_xenia.py
@@ -1,5 +1,13 @@
-# run_xenia.py
+import sys
 import subprocess
 
-cmd_xenia = "_xenia\\xenia_canary.exe _build\\xbox\\default_xenia.xex"
-subprocess.run(cmd_xenia, shell=True, cwd="..")
+sys.path.append("../dependencies/dev_scripts")
+
+from build_ark import build_patch_ark
+
+print("Building RB3DX for Xenia...")
+
+if build_patch_ark(True):
+    print("Ready to run RB3DX in Xenia.")
+    cmd_xenia = "_xenia\\xenia_canary.exe _build\\xbox\\default_xenia.xex"
+    subprocess.run(cmd_xenia, shell=True, cwd="..")
