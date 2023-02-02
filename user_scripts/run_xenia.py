a1006bd7705271b3ece0ead5417052d8c050c520
rjkiv <rjkiv@users.noreply.github.com>
2023-02-01 20:29:37 -0700

    print statements

--- user_scripts/run_xenia.py
+++ user_scripts/run_xenia.py
@@ -16,9 +16,13 @@ cmd_xenia = "_xenia\\xenia_canary.exe _build\\xbox\\default_xenia.xex"
 if check_git_updated:
     res = True
     if not root_dir.joinpath("_build/xbox/gen/patch_xbox_0.ark").is_file():
+        print("RB3DX ark not found, building it now...")
         res = build_patch_ark(True)
     if res:
+        print("Ready to run RB3DX in Xenia.")
         subprocess.run(cmd_xenia, shell=True, cwd="..")
 else:
+    print("Local repo out of date, pulling and building an updated RB3DX ark now...")
     if build_patch_ark(True):
+        print("Ready to run RB3DX in Xenia.")
         subprocess.run(cmd_xenia, shell=True, cwd="..")
