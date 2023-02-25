c14b788b5fb559252e2061abf2d28c83403bc3c5
rjkiv <rjkiv@users.noreply.github.com>
2023-02-25 15:17:06 -0700

    confirm reset in bat

--- windows_bats/git_reset.bat
+++ windows_bats/git_reset.bat
@@ -1,4 +1,6 @@
 @echo off
 cd "%~dp0../user_scripts"
+echo You are about to reset the repo. Close this window to abort, or press any key to proceed.
+PAUSE
 python "%~dp0../user_scripts/git_reset.py"
 PAUSE
