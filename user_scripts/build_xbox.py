51401ec1aec021f3e5645da79562a25a6f1f1139
Luna <luna.abarta@gmail.com>
2023-04-14 16:30:29 -0700

    remove dxsl

--- user_scripts/build_xbox.py
+++ user_scripts/build_xbox.py
@@ -2,7 +2,6 @@ import sys
 sys.path.append("../dependencies/dev_scripts")
 
 from download_mackiloha import download_mackiloha
-from build_dx_settings_ark import build_dxsl_ark
 from build_ark import build_patch_ark
 
 # Download and extract Mackiloha-suite-archive.zip
@@ -10,6 +9,5 @@ if not download_mackiloha():
     print("Failed to download and extract Mackiloha-suite-archive.zip. Exiting.")
     sys.exit(1)
 
-if build_dxsl_ark():
-    if build_patch_ark(True):
-        print("You may find the files needed to place on your Xbox 360 in /_build/Xbox/.")
+if build_patch_ark(True):
+    print("You may find the files needed to place on your Xbox 360 in /_build/Xbox/.")
--- user_scripts/build_xenia.py
+++ user_scripts/build_xenia.py
@@ -5,7 +5,6 @@ sys.path.append("../dependencies/dev_scripts")
 from download_mackiloha import download_mackiloha
 
 from download_xenia import setup_xenia
-from build_dx_settings_ark import build_dxsl_ark
 from build_ark import build_patch_ark
 
 # Download and extract Mackiloha-suite-archive.zip
@@ -13,9 +12,8 @@ if not download_mackiloha():
     print("Failed to download and extract Mackiloha-suite-archive.zip. Exiting.")
     sys.exit(1)
 
-if build_dxsl_ark():
-    if build_patch_ark(True):
-        print("Checking for updates to Xenia Canary")
-        setup_xenia()
-        cmd_xenia = "_xenia\\xenia_canary.exe _build\\xbox\\dx-settings-loader.xex"
-        subprocess.run(cmd_xenia, shell=True, cwd="..")
+if build_patch_ark(True):
+    print("Checking for updates to Xenia Canary")
+    setup_xenia()
+    cmd_xenia = "_xenia\\xenia_canary.exe _build\\xbox\\default.xex"
+    subprocess.run(cmd_xenia, shell=True, cwd="..")
