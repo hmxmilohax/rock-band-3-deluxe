ed48a1cc31a0662c2669d4fa2066065605fcca21
Luna <luna.abarta@gmail.com>
2023-04-14 18:14:21 -0700

    oops

--- user_scripts/run_xenia.py
+++ user_scripts/run_xenia.py
@@ -6,7 +6,6 @@ sys.path.append("../dependencies/dev_scripts")
 from download_xenia import setup_xenia
 from download_mackiloha import download_mackiloha
 from build_ark import build_patch_ark
-from build_dx_settings_ark import build_dxsl_ark
 from check_git_updated import check_git_updated
 
 # Download and extract Mackiloha-suite-archive.zip
@@ -18,17 +17,7 @@ if not download_mackiloha():
 cwd = Path(__file__).parent
 root_dir = Path(__file__).parents[1] # root directory of the repo
 
-cmd_xenia_loader = "_xenia\\xenia_canary.exe _build\\xbox\\dx-settings-loader.xex"
 cmd_xenia = "_xenia\\xenia_canary.exe _build\\xbox\\default.xex"
-
-dx_res = True
-if check_git_updated(repo_url="https://github.com/hmxmilohax/dx-settings-loader", repo_root_path=root_dir.joinpath("dependencies/dev_scripts/dx-settings-loader")):
-    if not root_dir.joinpath("_build/xbox/gen/dxsl_xbox_0.ark").is_file():
-        print("DXSL ark not found, building it now...")
-        dx_res = build_dxsl_ark()
-else:
-    print("Local repo out of date, pulling and building an updated DXSL ark now...")
-    dx_res = build_dxsl_ark()
 
 rb3dx_res = True
 if check_git_updated(repo_url="https://github.com/hmxmilohax/rock-band-3-deluxe", repo_root_path=root_dir):
@@ -39,11 +28,11 @@ else:
     print("Local repo out of date, pulling and building an updated Rock Band 3 Deluxe ark now...")
     rb3dx_res = build_patch_ark(True)
     
-if dx_res:
+if rb3dx_res:
     print("Checking for updates to Xenia Canary")
     setup_xenia()
-    print("Ready to run DXSL in Xenia.")
-    subprocess.run(cmd_xenia_loader, shell=True, cwd="..")
+    print("Ready to run Rock Band 3 Deluxe in Xenia.")
+    subprocess.run(cmd_xenia, shell=True, cwd="..")
 # if rb3dx_res:
 #     print("Ready to run Rock Band 3 Deluxe in Xenia.")
 #     subprocess.run(cmd_xenia, shell=True, cwd="..")
