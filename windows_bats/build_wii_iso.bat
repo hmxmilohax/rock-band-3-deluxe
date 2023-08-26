74b71e6ab22fe86717500403329e856d9a30cfbb
jnack <55568980+jnackmclain@users.noreply.github.com>
2023-08-25 19:00:59 -0500

    linux delete command in windows cmd :SaySike:

--- windows_bats/build_wii_iso.bat
+++ windows_bats/build_wii_iso.bat
@@ -1,5 +1,6 @@
 @echo off
 echo:
-rm "%~dp0../_build/wii/RB3Deluxe.iso"
+cd "%~dp0../_build/wii/"
+DEL /F RB3Deluxe.wbfs
 echo:Building Wii WBFS
-wit COPY "%~dp0../_build/wii/wit_input" "%~dp0../_build/wii/RB3Deluxe.iso"
+wit COPY "wit_input" "RB3Deluxe.iso"
