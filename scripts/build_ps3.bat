cefcb7003b0c1c2809cba24011e8ea2e40a7cdd2
LocalH <localhbci@gmail.com>
2023-10-16 15:16:32 -0400

    Updated build scripts to hide the dtb deletion spew

--- scripts/build_ps3.bat
+++ scripts/build_ps3.bat
@@ -1,6 +1,8 @@
 @echo off
 cd "%~dp0.."
-del /s /q obj\*.dtb
+echo Deleting old PS3 DTBs, if present...
+del >nul 2>&1 /s /q obj\ps3\*.dtb
+echo.
 python dependencies\python\configure_build.py ps3
 dependencies\windows\ninja
 PAUSE
