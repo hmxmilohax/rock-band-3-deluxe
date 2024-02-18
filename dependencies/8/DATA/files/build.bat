e08538be39fdaec816cdffdea094427ab2bdab40
jnack <55568980+jnackmclain@users.noreply.github.com>
2024-02-17 18:42:04 -0600

    add 8 for dev

---
+++ dependencies/8/DATA/files/build.bat
@@ -0,0 +1,5 @@
+del B:\git\rock-band-3-deluxe\dependencies\8\DATA\files\gen\main_wii.hdr
+del B:\git\rock-band-3-deluxe\dependencies\8\DATA\files\gen\main_wii_10.ark
+copy B:\git\rock-band-3-deluxe\dependencies\8\DATA\files\vanilla_hdr\main_wii.hdr B:\git\rock-band-3-deluxe\dependencies\8\DATA\files\gen\main_wii.hdr
+arkhelper patchcreator B:\git\rock-band-3-deluxe\dependencies\8\DATA\files\gen\main_wii.hdr B:\git\rock-band-3-deluxe\dependencies\8\DATA\sys\main.dol -a B:\git\rock-band-3-deluxe\dependencies\8\DATA\files\_ark -o B:\git\rock-band-3-deluxe\dependencies\8\DATA\files
+del B:\git\rock-band-3-deluxe\dependencies\8\DATA\files\main.dol
