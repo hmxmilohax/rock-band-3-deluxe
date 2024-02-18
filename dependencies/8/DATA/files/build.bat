15bf6fcf4c71662b1664d0605c6ff047cb072e88
jnack <55568980+jnackmclain@users.noreply.github.com>
2024-02-17 18:54:18 -0600

    better build bat

--- dependencies/8/DATA/files/build.bat
+++ dependencies/8/DATA/files/build.bat
@@ -1,5 +1,5 @@
-del B:\git\rock-band-3-deluxe\dependencies\8\DATA\files\gen\main_wii.hdr
-del B:\git\rock-band-3-deluxe\dependencies\8\DATA\files\gen\main_wii_10.ark
-copy B:\git\rock-band-3-deluxe\dependencies\8\DATA\files\vanilla_hdr\main_wii.hdr B:\git\rock-band-3-deluxe\dependencies\8\DATA\files\gen\main_wii.hdr
-arkhelper patchcreator B:\git\rock-band-3-deluxe\dependencies\8\DATA\files\gen\main_wii.hdr B:\git\rock-band-3-deluxe\dependencies\8\DATA\sys\main.dol -a B:\git\rock-band-3-deluxe\dependencies\8\DATA\files\_ark -o B:\git\rock-band-3-deluxe\dependencies\8\DATA\files
-del B:\git\rock-band-3-deluxe\dependencies\8\DATA\files\main.dol
+del "%~dp0gen\main_wii.hdr"
+del "%~dp0gen\main_wii_10.ark"
+copy "%~dp0vanilla_hdr\main_wii.hdr" "%~dp0gen\main_wii.hdr"
+"%~dp0..\..\..\windows\arkhelper.exe" patchcreator "%~dp0gen\main_wii.hdr" "%~dp0..\sys\main.dol" -a "%~dp0_ark" -o %~dp0
+del "%~dp0main.dol"
