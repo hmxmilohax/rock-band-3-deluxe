4c0078ad2df089c69549dca5f1ac7665406ae75f
jnack <55568980+jnackmclain@users.noreply.github.com>
2024-02-18 11:30:32 -0600

    clean stuff, add build wii bat

--- dependencies/8/DATA/files/build.bat
+++
@@ -1,5 +0,0 @@
-del "%~dp0gen\main_wii.hdr"
-del "%~dp0gen\main_wii_10.ark"
-copy "%~dp0vanilla_hdr\main_wii.hdr" "%~dp0gen\main_wii.hdr"
-"%~dp0..\..\..\windows\arkhelper.exe" patchcreator "%~dp0gen\main_wii.hdr" "%~dp0..\sys\main.dol" -a "%~dp0_ark" -o %~dp0
-del "%~dp0main.dol"
