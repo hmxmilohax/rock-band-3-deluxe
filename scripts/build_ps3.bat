d78d244a8bc11a35d53c66b99bb306baa0a2eee0
jnack <55568980+jnackmclain@users.noreply.github.com>
2024-03-01 15:10:28 -0600

    fix out of order

--- scripts/build_ps3.bat
+++ scripts/build_ps3.bat
@@ -4,6 +4,6 @@ del >nul 2>&1 /s /q obj\ps3\*.dtb
 python dependencies\python\add_devbuild.py
 python dependencies\python\configure_build.py ps3 --fun
 dependencies\windows\ninja
-if %errorlevel% neq 0 (pause /b %errorlevel% && exit /b %errorlevel%)
 python dependencies\python\remove_devbuild.py
+if %errorlevel% neq 0 (pause /b %errorlevel% && exit /b %errorlevel%)
 start "" "%~dp0..\out\ps3"
--- scripts/build_wii.bat
+++ scripts/build_wii.bat
@@ -4,6 +4,6 @@ del >nul 2>&1 /s /q obj\wii\*.dtb
 python dependencies\python\add_devbuild.py
 python dependencies\python\configure_build.py wii --fun
 dependencies\windows\ninja
-if %errorlevel% neq 0 (pause /b %errorlevel% && exit /b %errorlevel%)
 python dependencies\python\remove_devbuild.py
+if %errorlevel% neq 0 (pause /b %errorlevel% && exit /b %errorlevel%)
 start "" "%~dp0..\out\wii\files"
