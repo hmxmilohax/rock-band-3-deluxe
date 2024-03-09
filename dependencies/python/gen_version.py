fa5e0820ecba63c7dca7a14343023d0cfdd759d0
Dark <dark@dark.ski>
2024-03-09 17:27:37 -0500

    i may be stupid

--- dependencies/python/gen_version.py
+++ dependencies/python/gen_version.py
@@ -4,15 +4,15 @@ import subprocess
 import sys
 
 commit = subprocess.check_output(["git", "describe", "--always", "--dirty"],text=True).strip("\n")
-version = "1.1.0-nightly+{commit}"
+version = f"1.1.0-nightly+{commit}"
 
 path = sys.argv[1]
 
 f = open(path, "w")
 
-f.write(f'(message_motd "Rock Band 3 Deluxe 1.1.0-nightly+{commit} Loaded! Thanks for playing!")\n')
-f.write(f'(message_motd_signin "Rock Band 3 Deluxe 1.1.0-nightly+{commit} Loaded! Thanks for playing!")\n')
-f.write(f'(message_motd_noconnection "Rock Band 3 Deluxe 1.1.0-nightly+{commit} Loaded! Thanks for playing!")\n')
-f.write(f'(rb3e_mod_string "RB3DX 1.1.0-nightly+{commit}")\n')
+f.write(f'(message_motd "Rock Band 3 Deluxe {version} Loaded! Thanks for playing!")\n')
+f.write(f'(message_motd_signin "Rock Band 3 Deluxe {version} Loaded! Thanks for playing!")\n')
+f.write(f'(message_motd_noconnection "Rock Band 3 Deluxe {version} Loaded! Thanks for playing!")\n')
+f.write(f'(rb3e_mod_string "RB3DX {version}")\n')
 
 f.close()
